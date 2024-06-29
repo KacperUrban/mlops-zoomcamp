948bf7f11bbdc8c9631d05ff6622e494b55cca99
Kacper Urban <urbankacper613@gmail.com>
2024-06-29 18:18:29 +0200

    Wrote simple functions to do predictions

---
+++ 04-deployment/web-service/predict.py
@@ -0,0 +1,17 @@
+import pickle
+import numpy as np
+
+with open('lin_reg.bin', 'rb') as f_in:
+    (dv, model) = pickle.load(f_in)
+
+
+def prepare_features(ride):
+    features = {}
+    features['PU_Do'] = '%s_%s' % (ride['PULocationID'], ride['DOLocationID'])
+    features['trip_distance'] = ride['trip_distance']
+    return features
+
+def predict(features):
+    X = dv.transform(features)
+    preds = np.round(model.predict(X),3)
+    return preds[0]
